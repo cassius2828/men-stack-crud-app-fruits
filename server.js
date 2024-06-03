37b104bfb62f60361f4ec8736b87209ef8eda74b
Cassius <cassius.reynolds.dev@gmail.com>
2024-06-03 11:41:27 -0700

    added delete and edit requests for fruits. Issue earlier was not using the double bang on isReadyToEat checkbox and forgetting to put the name attr in the form inputs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const e = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const methodOverride = require("method-override");
 
 // setups
 dotenv.config();
@@ -20,7 +21,7 @@ mongoose.connect(MONGODB_URI);
 // this function will fire once a connection
 // between our express and mongdv atlas is confirmed
 mongoose.connection.on("connected", function () {
-//   console.log(`Connected to Mongodb ${mongoose.connection.name}`);
+  console.log(`Connected to Mongodb ${mongoose.connection.name}`);
 });
 
 ///////////////////////////
@@ -30,7 +31,8 @@ mongoose.connection.on("connected", function () {
 app.use(e.urlencoded({ extended: false }));
 // displays HTTP reqs in console/terminal
 app.use(morgan("dev"));
-
+// method overrides
+app.use(methodOverride("_method"));
 
 ///////////////////////////
 // http requests
@@ -42,20 +44,24 @@ app.get("/", (req, res) => {
 // Read Routes
 // Index Route - We want a page that we can view all the fruits
 
+// get all fruits
 app.get("/fruits", async (req, res) => {
   // go to db and get all fruits
   // display all fruits in index.ejs
 
   const allFruits = await FruitModel.find({});
-  console.log(allFruits);
+  // console.log(allFruits);
   res.render("fruits/index.ejs", { allFruits });
 });
 
+// render the new fruit page
 app.get("/fruits/new", (req, res) => {
   // res.render looks at views for template
+
   res.render("fruits/new.ejs");
 });
 
+// create a new fruit
 app.post("/fruits", async (req, res) => {
   //   if (req.body.isReadyToEat) {
   //     req.body.isReadyToEat = true;
@@ -68,17 +74,49 @@ app.post("/fruits", async (req, res) => {
   req.body.isReadyToEat = !!req.body.isReadyToEat;
   const fruit = await FruitModel.create(req.body);
   //   console.log(req.body);
-//   console.log(fruit);
+  //   console.log(fruit);
   res.redirect("/fruits");
 });
 
+// get fruit by id
 app.get("/fruits/:id", async (req, res) => {
   const { id } = req.params;
   const selectedFruit = await FruitModel.findById(id);
-//   console.log(selectedFruit, " <-- selected fruit");
+  //   console.log(selectedFruit, " <-- selected fruit");
   res.render("fruits/show.ejs", { selectedFruit });
 });
 
+// delete fruit
+app.delete("/fruits/:id", async (req, res) => {
+  const { id } = req.params;
+  const fruitToDelete = await FruitModel.deleteOne({ id });
+  console.log("deleted this fruit", fruitToDelete);
+  res.redirect("/fruits");
+});
+
+// show edit fruit
+app.get("/fruits/:id/edit", async (req, res) => {
+  const { id } = req.params;
+
+  const selectedFruit = await FruitModel.findById(id);
+
+  res.render("fruits/edit.ejs", { selectedFruit });
+});
+
+// edit fruit
+app.put("/fruits/:id/edit", async (req, res) => {
+  const { id } = req.params;
+
+  const selectedFruitInfo = {
+    name: req.body.name,
+    isFruitReadyToEat: !!req.body.isFruitReadyToEat,
+  };
+
+  const fruitToEdit = await FruitModel.findByIdAndUpdate(id, selectedFruitInfo);
+  // console.log('Updated fruit', fruitToEdit)
+  res.redirect("/fruits");
+});
+
 app.listen(3000, () => {
-//   console.log("listening on port 3000");
+  //   console.log("listening on port 3000");
 });
